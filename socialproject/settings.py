f8b8bfff3b113bde32b7a62353134cd55f5f7745
Andy Osborne <andyjamesosborne@proton.me>
2024-03-11 13:31:04 +0000

    allowed host was wrong for heroku

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.gitpod.io','socialproject.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com']
 
 
 # Application definition
