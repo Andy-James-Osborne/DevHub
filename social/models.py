ebd0579d64d68b7299ab72770cd8a95c1dc57816
Andy Osborne <andyjamesosborne@proton.me>
2024-03-12 19:20:08 +0000

    Sorted the post feed page and view post to comment

--- social/models.py
+++ social/models.py
@@ -4,14 +4,43 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model() #Get current user
 
+STATUS = ((0, "Draft"), (1, "Published"))
 
 class Profile(models.Model):
+    title = models.CharField(max_length=200, unique=True, default="")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
+    slug = models.SlugField(max_length=200, unique=True, default="", null=False)
     bio = models.TextField(blank=True)
-    profliepic = models.ImageField(upload_to='profile_images', default='5907.jpg')
     profile_picture = CloudinaryField('profile_pictures', default='placeholder', blank=True)
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.title
+    
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    class Meta:      
+        ordering = ["created_on"]
+
+    def __str__(self):     
+        return f"Comment {self.body} by {self.name}"
--- social/views.py
+++ social/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profile, Post, Comment
+from .forms import CommentForm
+from django.http import HttpResponseRedirect
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render(request, 'social/home.html')
+    posts = Post.objects.all().order_by("created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, 'social/home.html', context)
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": CommentForm(),
+    }
+
+    return render(request, "social/post_detail.html", context)
