5f4daac1a0a738402d56d6664ddcf229f402b888
Andy Osborne <andyjamesosborne@proton.me>
2024-03-12 19:42:51 +0000

    User auth added with login required

--- social/views.py
+++ social/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Profile, Post, Comment
 from .forms import CommentForm
 from django.http import HttpResponseRedirect
-# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
@@ -13,7 +13,7 @@ def home(request):
     }
     return render(request, 'social/home.html', context)
 
-
+@login_required(login_url="login")
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     form = CommentForm()
@@ -35,3 +35,32 @@ def post_detail(request, pk):
     }
 
     return render(request, "social/post_detail.html", context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    context = {'form' : form}
+    return render(request, 'social/signup.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    else: 
+        form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'social/login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
