db8c253a32dcecc6f89125f47c1cb5011dfd736b
Andy Osborne <andyjamesosborne@proton.me>
2024-03-13 09:59:14 +0000

    edit nav bar to only display log out when logged in

--- social/models.py
+++ social/models.py
@@ -9,7 +9,6 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Profile(models.Model):
     title = models.CharField(max_length=200, unique=True, default="")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    slug = models.SlugField(max_length=200, unique=True, default="", null=False)
     bio = models.TextField(blank=True)
     profile_picture = CloudinaryField('profile_pictures', default='placeholder', blank=True)
     location = models.CharField(max_length=100, blank=True)
@@ -17,21 +16,24 @@ class Profile(models.Model):
     def __str__(self):
         return self.title
     
-    def number_of_likes(self):
-        return self.likes.count()
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    class Meta:
+        ordering = ["created_on"]
 
     def __str__(self):
         return self.title
 
+    def number_of_likes(self):
+        return self.likes.count()
 
 class Comment(models.Model):
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
@@ -43,4 +45,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):     
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.body} by {self.author}"
