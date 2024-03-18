3ef292e13ab8a86b426b7d7742b360832000a791
Andy Osborne <andyjamesosborne@proton.me>
2024-03-18 18:07:42 +0000

    User profile able to be seen by other users

--- social/models.py
+++ social/models.py
@@ -45,3 +45,8 @@ class Comment(models.Model):
 
     def __str__(self):     
         return f"Comment {self.body} by {self.author}"
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
